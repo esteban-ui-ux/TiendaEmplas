ad778ac4fbf619d27197f21be7c102985972c45c
Esteban <esteban.ui.ux@gmail.com>
2022-11-23 00:17:01 -0500

    Cambios estilos
    Para que se vea más bonito

--- front/src/components/layout/Footer.js
+++ front/src/components/layout/Footer.js
@@ -1,14 +1,13 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 export const Footer = () => {
   return (
     <Fragment>
-        <footer className="py-1">
-            <p className="text-center mt-1">
-                Vety Shop Store - 2022 - Mision TIC - Todos los derechos reservados
-            </p>
-        </footer>
-
+      <footer className="py-1">
+        <p className="text-center mt-1">
+          Pentágono el mejor equipo - Mision TIC - Universidad de Antioquia
+        </p>
+      </footer>
     </Fragment>
-  )
-}
+  );
+};
